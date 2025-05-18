5a77a1fbd1895730d98de418810af236b8c5f78a
Mihalache Mihai <mihai.mihalache2002@student.tuiasi.ro>
2025-05-18 10:15:33 +0300

    Add supabase config

--- backend/config/config.ts
+++ backend/config/config.ts
@@ -1,4 +1,11 @@
 import dotenv from 'dotenv';
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+
+export const supabase = createClient(supabaseUrl!, supabaseKey!)
+
 
 dotenv.config();
 
